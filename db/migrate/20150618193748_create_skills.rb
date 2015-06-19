55903ae0941d86452615469fc3c540e453d8840d
costolo <ben.costolo@gmail.com>
2015-06-19 10:45:58 -0400

    Add defaults to skills migration

--- db/migrate/20150618193748_create_skills.rb
+++ db/migrate/20150618193748_create_skills.rb
@@ -2,8 +2,8 @@ class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
       t.string :title
-      t.integer :current_streak
-      t.integer :longest_streak
+      t.integer :current_streak, default: 1
+      t.integer :longest_streak, default: 1
       t.references :user
 
       t.timestamps
