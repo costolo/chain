3fe43fbe4d021a554b7e4d344bb4e498e976f3e2
costolo <ben.costolo@gmail.com>
2015-06-24 11:38:20 -0400

    Add additional validations to Skill table

--- db/migrate/20150618193748_create_skills.rb
+++ db/migrate/20150618193748_create_skills.rb
@@ -1,9 +1,11 @@
 class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
-      t.string :title
+      t.string :title, null: false
       t.integer :current_streak, default: 1
       t.integer :longest_streak, default: 1
+      t.integer :refreshed_at
+
       t.references :user
 
       t.timestamps
