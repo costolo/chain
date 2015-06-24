e6adb51305e6b6a5b279019e86aec6efd275293a
costolo <ben.costolo@gmail.com>
2015-06-24 11:38:05 -0400

    Add set_refreshed_at method to be called before saving of new records

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,9 @@
 class Skill < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
+  before_save :set_refreshed_at
+
+  def set_refreshed_at
+    self.new_record? ? self.refreshed_at = self.created_at.to_i : self
+  end
 end
