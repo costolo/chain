15a8670a0f53b096cb5633c4cbc011f68172bbe3
costolo <ben.costolo@gmail.com>
2015-06-29 11:05:06 -0400

    Add test that verifies the correct time is associated with the skill record

--- spec/model_specs/skill_spec.rb
+++ spec/model_specs/skill_spec.rb
@@ -5,8 +5,14 @@ describe Skill do
     skill = Skill.new(title: "juggling")
     expect(skill.valid?).to be true
   end
+
   it "is invalid without a title" do
     skill = Skill.new(title: nil)
     expect(skill.errors[:title]).not_to include("can't be blank")
+  end
+
+  it "sets refreshed_at to the current epoch time upon record creation" do
+    skill = Skill.create(title: "hello there")
+    expect(skill.refreshed_at).to eq Time.now.to_i
   end
 end
