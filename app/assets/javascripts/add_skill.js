243949b415c875f56f01ef2793ebd23e4ddc88a4
costolo <ben.costolo@gmail.com>
2015-07-02 16:56:42 -0400

    Add semicolons and add temporary string for time remaining

--- app/assets/javascripts/add_skill.js
+++ app/assets/javascripts/add_skill.js
@@ -2,16 +2,16 @@ function addSkill() {
   $("#new_skill").on("submit", function (event) {
     event.preventDefault();
     var $target = $(event.target);
-    console.log($target)
+    console.log($target);
     if ($('#skill_title').val() === "") {
-      alert("No skill specified")
+      alert("No skill specified");
     } else {
       $.ajax({
         url: $target.attr("action") + '.json',
         type: "post",
         data: $target.serialize()
       }).done(function (response) {
-        $('.skills').append(response.title + "<br>");
+        $('.skills').append(response.title + " - Time remaining: 24:00:00" + "<br>");
         $target[0][2].value = "";
       });
     }
