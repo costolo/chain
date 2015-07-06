c9a06e4b061b700f338bdd92679a84d63905b3b4
costolo <ben.costolo@gmail.com>
2015-07-06 14:19:34 -0400

    Change default of current_streak to 1

--- db/migrate/20150618193748_create_skills.rb
+++ db/migrate/20150618193748_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
       t.string :title, null: false
-      t.integer :current_streak, default: 0
+      t.integer :current_streak, default: 1
       t.integer :longest_streak, default: 1
       t.integer :expiration_time
 
