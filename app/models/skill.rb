5b9f979fd0fd45101d3caed8081c86a7f9fb150c
costolo <ben.costolo@gmail.com>
2015-07-06 14:18:51 -0400

    Attempt to make methods more single responsibility

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -12,19 +12,30 @@ class Skill < ActiveRecord::Base
   def end_current_streak
     self.current_streak = 0
     self.save
+    "Chain broken"
   end
 
   def refresh_expiration_time
     self.expiration_time = Time.now.to_i + 86400
+    self.save
+    increment_current_streak
+  end
+
+  def increment_current_streak
+    self.current_streak += 1
     self.save
   end
 
   def time_remaining
     sec_remaining = self.expiration_time - Time.now.to_i
     if sec_remaining > 0
-      "Time remaining: #{Time.at(sec_remaining).utc.strftime("%H:%M:%S")}"
+      time_remaining_to_s(sec_remaining)
     else
-      "Chain broken"
+      self.end_current_streak
     end
+  end
+
+  def time_remaining_to_s(sec_remaining)
+    "Time remaining: #{Time.at(sec_remaining).utc.strftime("%H:%M:%S")}"
   end
 end
