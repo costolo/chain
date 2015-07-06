5ca6af74e2b5866fd81e0e2744cb2d8d78cff724
costolo <ben.costolo@gmail.com>
2015-07-06 16:45:56 -0400

    Add countdown function for jquery countdown

---
+++ app/assets/javascripts/countdown.js
@@ -0,0 +1,8 @@
+function countdown () {
+  $('[data-countdown]').each(function() {
+    var $this = $(this), finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function(event) {
+      $this.html(event.strftime('%H:%M:%S'));
+    });
+  });
+}
