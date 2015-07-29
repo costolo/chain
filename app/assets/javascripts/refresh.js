b95a5171ad30d9f6eec3608654ad336ec83eba8e
costolo <ben.costolo@gmail.com>
2015-07-29 16:30:53 -0400

    Establish variables for target, url, skill id, 24h datetime, and span. Then update span's data-countdown attribute and call countdown to refresh the countdown to twenty four hours

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -2,11 +2,16 @@ function refresh () {
   $(".refresh-link").on("click", function(event) {
     event.preventDefault();
     var $target = $(event.target);
+    var url = $target.attr("href");
+    var id = url.match(/(\d+)(?!.*\d)/)[0].toString();
+    var dateTimeIn24Hours = new Date();
+    var $dateSpan = $(".data" + id);
     $.ajax({
-      url: $target.attr("href"),
+      url: url,
       type: "get"
     }).done(function(response) {
-      
+      $dateSpan.data("countdown", formatCurrentDateTime(dateTimeIn24Hours));
+      countdown();
     });
   });
 }
