82380cd4ac8c2bc6b374d8a6850d6a44e96114e2
costolo <ben.costolo@gmail.com>
2015-07-30 16:42:32 -0400

    append instead of replace contents of modal to preserve close button

--- app/assets/javascripts/getAuth.js
+++ app/assets/javascripts/getAuth.js
@@ -3,15 +3,14 @@ function getAuth() {
     url: "/users/sign_in",
     type: "get",
   }).done(function(signin_response) {
-    $(".modal-1-inner").html(signin_response);
-    console.log(signin_response);
+    $(".modal-1-inner").append(signin_response);
   });
 
   $.ajax({
     url: "/users/sign_up",
     type: "get"
   }).done(function(signup_response) {
-    $(".modal-2-inner").html(signup_response);
+    $(".modal-2-inner").append(signup_response);
   });
 
   $(".sign-in-link").click(function(event) {
