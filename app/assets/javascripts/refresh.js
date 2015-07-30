0c401d507b47209d52598544c71d9a6e046bd1b2
costolo <ben.costolo@gmail.com>
2015-07-30 14:47:04 -0400

    Add variables to grab the timeRemainingSpan, current streak integer, and longest streak integer

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -6,11 +6,14 @@ function refresh () {
     var id = url.match(/(\d+)(?!.*\d)/)[0].toString();
     var dateTimeIn24Hours = new Date();
     var $dateSpan = $(".data" + id);
+    var $timeRemainingSpan = $(".time-remaining" + id);
+    var longest = parseInt($("#longest" + id).text(), 10);
+    var current = parseInt($("#current" + id).text(), 10);
+    
     $.ajax({
       url: url,
       type: "get"
     }).done(function(response) {
-      $dateSpan.data("countdown", formatCurrentDateTime(dateTimeIn24Hours));
       countdown();
     });
   });
