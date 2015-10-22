4f0cda417626cee30788fb6dafe965ba4a1f61ca
costolo <ben.costolo@gmail.com>
2015-10-22 16:04:43 -0400

    Update refresh route to make restful

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', sessions: 'sessions' }
-  resources :skills
+  resources :skills do
+    get 'refresh' => 'skills#refresh'
+  end
 
   root 'skills#index'
-  get 'refresh' => 'skills#refresh', as: 'refresh'
   get 'welcome' => 'skills#splash', as: 'welcome'
 end
